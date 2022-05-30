7c1364f376c99b27a6758caefde766b117e53aec
TaikiHibino <101500265+TaikiHibino@users.noreply.github.com>
2022-05-30 23:17:21 +0900

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
-import re
 
 #計算をする関数
 def keisann(a,b,c):
@@ -25,4 +21,3 @@ temporary_substitution = [tmp.append(keisann(tmp[-1],num[i+1],fu[i])) for i in r
 #答えを出力
 st.write("答え")
 st.write(str(tmp[-1]))
-st.markdown(tmp[-1])
